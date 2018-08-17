32d30d4cb952250d5c4f07dfc34a4b2269eb8954
Wolfgang Kapplmüller <w.kapplmueller@gmx.at>
2018-08-17 22:08:51 +0200

    Add first UI elements

--- app/src/main/java/at/kappw/vietnamesevocab/Home.java
+++ app/src/main/java/at/kappw/vietnamesevocab/Home.java
@@ -1,7 +1,9 @@
 package at.kappw.vietnamesevocab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+    }
+
+    public void startTest(View view) {
+        Intent intent = new Intent(this, Train.class);
+        startActivity(intent);
     }
 }
